890c94bcbd2b8120f47edb51201916a4ba60db7d
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2017-10-04 01:40:06 +0800

    Implement dumb reconstruction of chord notes construction

---
+++ questions/chords.rb
@@ -0,0 +1,66 @@
+class Chords < Questions::Interface
+  ################ questions
+  def self.questions
+    [:notes_in_chords]
+  end
+  def self.question
+    send(questions.sample)
+  end
+
+  def self.notes_in_chords
+    key, mode, degree = keys[keys.keys.sample], chord_modes.sample, chord_degrees.sample
+    puts "--------------------------------"
+    puts "CHORDS - notes of chords given key"
+    puts "--------------------------------"
+    puts "===> Q: Find notes in the chords of:"
+    puts "  #{[key.sample, mode, degree].map(&:capitalize).join('  ')}"
+    gets
+    chord = send(degree, send(mode, key)).map { |e| e.join('/') }.join('  ')
+    puts "===> A: #{chord}"
+    puts
+  end
+
+  private
+
+  # registrars
+  def self.chord_modes
+    [:major, :minor, :dominant]
+  end  
+  def self.chord_degrees
+    [:seventh, :ninth, :eleventh, :thirteenth]
+  end
+
+  # main constructor
+  def self.chord_notes_of_mode(key, mode, inversion: 0)
+    k_idx = semitones_inference(keys)[key.sample].first
+    scale_notes = scale_in_mode(mode).map { |i| keys[(k_idx + i) % 12] }
+    # TODO: implement inversions
+    scale_notes.select.with_index { |_, i| i.even? } +
+      scale_notes.select.with_index { |_, i| i.odd? }
+  end
+  
+  # mode of chords
+  def self.major(key)
+    chord_notes_of_mode(key, 'Ionian')
+  end
+  def self.minor(key)
+    chord_notes_of_mode(key, 'Aeolian')
+  end
+  def self.dominant(key)
+    chord_notes_of_mode(key, 'Mixolydian')
+  end
+  
+  # degree of chords
+  def self.seventh(chord)
+    chord[0..3]
+  end
+  def self.ninth(chord)
+    chord[0..4]
+  end
+  def self.eleventh(chord)
+    chord[0..5]
+  end
+  def self.thirteenth(chord)
+    chord[0..6]
+  end
+end
